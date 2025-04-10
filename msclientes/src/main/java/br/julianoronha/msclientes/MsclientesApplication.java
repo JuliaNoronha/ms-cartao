14a6bb78987959ff68c536b75a694a5980d8fb12
julianoronha <devjulianoronha@gmail.com>
2025-04-10 18:33:30 -0300

    Feat: mscloudgateway e msclientes

--- msclientes/src/main/java/br/julianoronha/msclientes/MsclientesApplication.java
+++ msclientes/src/main/java/br/julianoronha/msclientes/MsclientesApplication.java
@@ -1,13 +1,13 @@
-package com.eurekaserver;
+package br.julianoronha.msclientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EurekaserverApplication {
+public class MsclientesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(MsclientesApplication.class, args);
 	}
 
 }
